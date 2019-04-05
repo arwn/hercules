15e937800ecb45b39fcf9b5bc962145613d2c9c4
arwn <windham.aren@protonmail.com>
2019-04-04 17:10:47 -0700

    sort headers

--- 05_stymphalian_birds/main.c
+++ 05_stymphalian_birds/main.c
@@ -1,12 +1,12 @@
+#include <ctype.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <ctype.h>
-#include <pwd.h>
-#include <stdbool.h>
 
 struct proj {
 	char *name;

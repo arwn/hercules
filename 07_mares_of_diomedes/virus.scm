5131acc34a9f7e10d532d8eec8cf38aea9ad961e
aren windham <heh@localhost.localdomain>
2019-04-20 22:10:23 -0700

    make virus more sneaky

--- 07_mares_of_diomedes/virus.scm
+++ 07_mares_of_diomedes/virus.scm
@@ -9,25 +9,41 @@
     "\033[8m" ;concealed on
 
     "\033[30m" ;black
-    "\033[31m" ;red
-    "\033[32m" ;green
-    "\033[33m" ;yellow
-    "\033[34m" ;blue
-    "\033[35m" ;magenta
-    "\033[36m" ;cyan
-    "\033[37m" ;white
+    ;"\033[31m" ;red
+    ;"\033[32m" ;green
+    ;"\033[33m" ;yellow
+    ;"\033[34m" ;blue
+    ;"\033[35m" ;magenta
+    ;"\033[36m" ;cyan
+    ;"\033[37m" ;white
 
-    "\033[40m" ;black
-    "\033[41m" ;red
-    "\033[42m" ;green
-    "\033[43m" ;yellow
-    "\033[44m" ;blue
-    "\033[45m" ;magenta
-    "\033[46m" ;cyan
-    "\033[47m" ;white
+    ;"\033[40m" ;black
+    ;"\033[41m" ;red
+    ;"\033[42m" ;green
+    ;"\033[43m" ;yellow
+    ;"\033[44m" ;blue
+    ;"\033[45m" ;magenta
+    ;"\033[46m" ;cyan
+    ;"\033[47m" ;white
 
     "\033[=0h" ;40x25 monochrome
+
+    "\033[A" ;cursor up one line
+    "\033[B" ;cursor down one line
+    "\033[C" ;cursor right one char
+    "\033[D" ;cursor left one char
+    "\033[I" ;reverse line feed
+    "\033[H" ;cursor to upper left corner
+    "\033[I" ;reverse line feed
+    "\033[H" ;cursor to upper left corner
+
+    "\033[0A" ;arrow up
+    "\033[0B" ;arrow down
+    "\033[0C" ;arrow right
+    "\033[0D" ;arrow left
+
     "\b" ;bell
+    "\t" ;tab
     ))
 
 (define (random-escape)
@@ -36,7 +52,7 @@
 (define (annoy)
   (display (random-escape))
   (flush-output) ;otherwise sleep hangs
-  (sleep 1)
+  (sleep 10)
   (annoy))
 
 (if (= 0 (process-fork))
